c4d6fdb7d542a0ec2b85c45c427bd26bead18d9f
kethana jakkamputi <kethanajakkamputi@gmail.com>
2026-06-16 19:29:16 +0530

    Add string processing class with special operations
    Implement string processing with special operations.

---
+++ Process String with Special Operations I.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def processStr(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        result = []
+        
+        for ch in s:
+            if ch.islower():
+                result.append(ch)
+            elif ch == '*':
+                if result:   
+                    result.pop()
+            elif ch == '#':
+                result.extend(result)   
+            elif ch == '%':
+                result.reverse()   
+        
+        return ''.join(result)
